79341ff954e18f8a5cf0d82f7025078eba2783c0
Timur Musin <tima96@bk.ru>
2023-06-22 22:42:22 +0300

    Initial commit

---
+++ Source/SnakeGame/Bomb.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bomb.h"
+#include "SnakeBase.h"
+
+// Sets default values
+ABomb::ABomb()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABomb::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABomb::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABomb::Interact(AActor* Interactor, bool bIsHead)
+{
+	if (bIsHead)
+	{
+		auto Snake = Cast<ASnakeBase>(Interactor);
+		if (IsValid(Snake))
+		{
+			Snake->Destroy();
+		}
+	}
+}
+
